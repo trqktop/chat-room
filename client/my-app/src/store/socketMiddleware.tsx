edfb2eed04a5e5f60601b381bad351ef2d7eaa75
Shirin Pavel <trqktop@yandex.ru>
2023-03-28 21:12:18 +0300

    add middleware

--- client/my-app/src/store/socketMiddleware.tsx
+++ client/my-app/src/store/socketMiddleware.tsx
@@ -1,13 +1,54 @@
-import { connect, disconnect } from './store';
+import { io } from "socket.io-client";
 
+const toggle = true;
 
+let actions = [
+    {
+        reducerAction: "chat/activeUsers",
+        socketAction: "chat/activeUsers",
+    },
+    {
+        reducerAction: "chat/updateMessages",
+        socketAction: "chat/updateMessages",
+        // socketAction: "send_message",
+    },
+    {
+        reduceAtion: "chat/userName",
+        socketAction: "chat/userName",
+    },
+];
+let emitsActions: any = {
+    "chat/AddMessage": "chat/AddMessage",
+    "chat/userName": "chat/userName",
+};
 
+export const createMySocketMiddleware = () => {
+    return (params: any) => {
+        let socket = io();
+        actions.forEach(({ reducerAction, socketAction }) => {
+            socket.on(socketAction, (data) => {
+                params.dispatch({
+                    type: reducerAction,
+                    payload: data,
+                });
+            });
+        });
 
+        socket.on("connect_error", (err) => {
+            console.log(err);
+        });
 
-
-export const soketMideware = (state: any) => (next: any) => (action: any) => {
-    const { chat } = state.getState()
-    const { events, users }: any = { ...chat }
-    const { isConnect }: any = { ...events }
-    next(action)
-}
+        return (next: any) => (action: any) => {
+            const { chat } = params.getState();
+            const emitActionType = emitsActions[action.type];
+            if (emitActionType) {
+                try {
+                    socket.emit(emitActionType, action.payload, chat.myName);
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+            return next(action);
+        };
+    };
+};
--- server/server.js
+++ server/server.js
@@ -43,33 +43,6 @@ io.on("connection", (socket) => {
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-
-
-
-
-
-
-// io.on('connection', (socket) => {
-//     socket.broadcast.emit('hi');
-// });
-// io.on('connection', (socket) => {
-//     socket.on('chat message', (msg) => {
-//         console.log('message: ' + msg);
-//     });
-// });
-// io.on('connection', (socket) => {
-//     socket.on('chat message', (msg) => {
-//         io.emit('chat message', msg);
-//     });
-// });
-// io.on('connection', (socket) => {
-//     console.log('a user connected');
-//     socket.on('disconnect', () => {
-//         console.log('user disconnected');
-//     });
-// });
-
 server.listen(3000, () => {
     console.log('listening on *:3000');
 });

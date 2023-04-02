7f672b53d7d9f6664d71af5b2b0561bf25704a48
Shirin Pavel <trqktop@yandex.ru>
2023-04-02 22:48:35 +0300

    add:skeleton

--- client/my-app/src/components/Messages/index.tsx
+++ client/my-app/src/components/Messages/index.tsx
@@ -31,6 +31,7 @@ const Messages = () => {
           </li>
         );
       })}
+
     </ul>
   );
 };
---
+++ client/my-app/src/components/Skeleton/index.tsx
@@ -0,0 +1,12 @@
+import './Skeleton.css'
+const Skeleton = () => {
+    return (
+        <>
+            <div className="card loading">
+                    <div className="description"></div>
+            </div>
+        </>
+    )
+}
+
+export default Skeleton
